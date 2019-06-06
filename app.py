76f7c16141dd9b94a8e94c08d0e08b824742c7a2
Aashita Arora <aashita.arora.2365@gmail.com>
2019-06-06 09:56:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request,render_template
+import wikipedia
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    if(request.method=='GET'):
+        return render_template("form.html")
+    else:
+       query=request.form.get('querystring')
+       try:
+           summary = wikipedia.summary(query)
+       except wikipedia.exceptions.DisambiguationError as e:
+             return render_template("result.html",message=e.options)
+       except wikipedia.exceptions.PageError as p:
+           return render_template("result.html",message="The page you requested was not found") 
+       else:
+            return render_template("result.html",message=summary)
+
+
+
+if __name__ == '__main__':
+   app.run()
